bba3a4c826faa92ee22fb5b0d301d69051421689
ahmad.mohammadi <a.mohammadi1026@gmail.com>
2022-11-21 05:41:29 +0330

    add header login

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+      "./pages/**/*.{js,ts,jsx,tsx}",
+      "./components/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+      extend: {},
+      colors:{
+        primary:'#2FD1C5',
+        login_bg:'#F5FBFF',
+        border_bg:'#E4EDFF',
+        primaryText:'#00394C',
+        secondaryText:'#2F394B'
+
+
+      },
+      fontFamily:{
+        body:['Nunito']
+      }
+    },
+    plugins: [],
+  }
